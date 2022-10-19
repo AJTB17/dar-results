6fa55892049f702074165c4168fb726fdf3be56a
Alfredo Tiapa <57116402+crypto-verga@users.noreply.github.com>
2022-10-19 11:38:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css'
+import * as XLSX from 'xlsx/xlsx.mjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const XLSX = require('xlsx')
+export default function ExpExcel(){
+    const excel = XLSX.readFile(
+        'C:\\Users\\Tiapa\\Desktop\\prueba\\datos.xlsx'
+    )
+    const nombreHoja = excel.SheetNames
+    const datos = XLSX.utils.sheet_to_json(excel.Sheets[nombreHoja[0]])
+    console.log(datos)
+
+    return <div>{
+      <p>{datos}</p>
+  }</div>
 }
 
-export default App;
+ExpExcel()
